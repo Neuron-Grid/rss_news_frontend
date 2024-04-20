2c8bd142925f0ee585637e0cbcd5a1be272ec714
Neuron Grid <neuron-grid@neuron-grid.net>
2024-04-20 13:30:21 +0900

    スプラッシュ画面を実装しました。 ログインページを作成しました。

--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:rss_news/login_validate.dart';
 
-void main() => runApp(const MyApp());
+void main() {
+  runApp(const MyApp());
+}
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: const Text('Stateful Widget')),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const Text('You have pushed the button this many times:'),
-              Text(
-                '$_counter',
-                style: Theme.of(context).textTheme.headlineMedium,
-              ),
-              ElevatedButton(
-                // RaisedButtonをElevatedButtonに変更
-                onPressed: _incrementCounter,
-                child: const Text('Increment Counter'),
-              ),
-            ],
-          ),
-        ),
-      ),
+    return const MaterialApp(
+      title: 'JboyApp',
+      home: AuthPage(),
     );
   }
 }
